20baed73581df480171ec4f73f431b045b963e7d
talipcr <talip.cesur@gmail.com>
2020-12-13 00:30:02 +0100

    fix ngxs

--- src/app/shared/store/fut/fut.state.ts
+++ src/app/shared/store/fut/fut.state.ts
@@ -5,89 +5,91 @@ import { of } from 'rxjs';
 import { ApiService } from 'src/app/core/services/api.service';
 import { FutStateModel, TeamModel } from './fut.model';
 import {
-  GetAllTeams,
-  AddPlayer,
-  ModifyPlayer,
-  SetCurrentPlayer,
-  DeletePlayer
+	GetAllTeams,
+	AddPlayer,
+	ModifyPlayer,
+	SetCurrentPlayer,
+	DeletePlayer,
 } from './fut.action';
+import { Injectable } from '@angular/core';
 
 @State<FutStateModel>({
-  name: 'futStateModel',
-  defaults: {
-    teams: null,
-    currentPlayer: null
-  }
+	name: 'futStateModel',
+	defaults: {
+		teams: null,
+		currentPlayer: null,
+	},
 })
+@Injectable()
 export class FutState {
-  constructor(private service: ApiService) {}
+	constructor(private service: ApiService) {}
 
-  ngxsOnInit(ctx: StateContext<FutStateModel>) {
-    ctx.dispatch(new GetAllTeams());
-  }
+	ngxsOnInit(ctx: StateContext<FutStateModel>) {
+		ctx.dispatch(new GetAllTeams());
+	}
 
-  @Action(GetAllTeams)
-  getAllTeams(ctx: StateContext<FutStateModel>) {
-    return this.service.getAllTeams().subscribe((data: TeamModel) => {
-      if (data) {
-        ctx.patchState({
-          teams: Object.assign([], data)
-        });
-      }
-    });
-  }
+	@Action(GetAllTeams)
+	getAllTeams(ctx: StateContext<FutStateModel>) {
+		return this.service.getAllTeams().subscribe((data: TeamModel) => {
+			if (data) {
+				ctx.patchState({
+					teams: Object.assign([], data),
+				});
+			}
+		});
+	}
 
-  @Action(AddPlayer)
-  addPlayer(ctx: StateContext<FutStateModel>, action: AddPlayer) {
-    const state = ctx.getState();
-    state.teams[0].players = [...state.teams[0].players, action.player];
-    ctx.patchState(state);
+	@Action(AddPlayer)
+	addPlayer(ctx: StateContext<FutStateModel>, action: AddPlayer) {
+		const state = ctx.getState();
+		state.teams[0].players = [...state.teams[0].players, action.player];
+		ctx.patchState(state);
 
-    // Add in json
-    return this.service.addPlayer(action.teamId, state.teams[0]);
-  }
+		// Add in json
+		return this.service.addPlayer(action.teamId, state.teams[0]);
+	}
 
-  @Action(ModifyPlayer)
-  modifyPlayer(ctx: StateContext<FutStateModel>, action: AddPlayer) {
-    const state = ctx.getState();
-    state.teams[0].players.forEach(player => {
-      if (player.id == action.player.id) player = action.player;
-    });
-    ctx.patchState(state);
+	@Action(ModifyPlayer)
+	modifyPlayer(ctx: StateContext<FutStateModel>, action: AddPlayer) {
+		const state = ctx.getState();
+		state.teams[0].players.forEach((player) => {
+			if (player.id == action.player.id) player = action.player;
+		});
+		ctx.patchState(state);
 
-    // Add in json
-    return this.service.addPlayer(action.teamId, state.teams[0]);
-  }
+		// Add in json
+		return this.service.addPlayer(action.teamId, state.teams[0]);
+	}
 
-  @Action(SetCurrentPlayer)
-  setCurrentPlayer(ctx: StateContext<FutStateModel>, action: SetCurrentPlayer) {
-    ctx.setState(
-      patch({
-        currentPlayer: action.player
-      })
-    );
-  }
+	@Action(SetCurrentPlayer)
+	setCurrentPlayer(ctx: StateContext<FutStateModel>, action: SetCurrentPlayer) {
+		ctx.setState(
+			patch({
+				currentPlayer: action.player,
+			})
+		);
+	}
 
-  @Action(DeletePlayer)
-  deletePlayer(ctx: StateContext<FutStateModel>, action: DeletePlayer) {
-    const state = ctx.getState();
-    state.teams[0].players = [
-      ...state.teams[0].players.filter(p => p.playerId !== action.playerId)
-    ];
-    ctx.patchState(state);
+	@Action(DeletePlayer)
+	deletePlayer(ctx: StateContext<FutStateModel>, action: DeletePlayer) {
+		const state = ctx.getState();
+		state.teams[0].players = [
+			...state.teams[0].players.filter((p) => p.playerId !== action.playerId),
+		];
+		ctx.patchState(state);
 
-    return this.service
-      .addPlayer(action.teamId, state.teams[0])
-      .pipe(
-        map(data => {
-          console.log(data);
-        }),
-        catchError(error => of(console.log('error'))),
-        finalize(() => ctx.dispatch(new GetAllTeams()))
-      )
-      .subscribe(() => {
-        state.currentPlayer = null;
-        ctx.patchState(state);
-      });
-  }
+		return this.service
+			.addPlayer(action.teamId, state.teams[0])
+			.pipe(
+				map((data) => {
+					console.log(data);
+				}),
+				catchError((error) => of(console.log('error'))),
+				finalize(() => ctx.dispatch(new GetAllTeams()))
+			)
+			.subscribe(() => {
+				state.currentPlayer = null;
+				ctx.patchState(state);
+			});
+	}
 }
