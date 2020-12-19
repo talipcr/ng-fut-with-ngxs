0a5e7bfc87007c2c0f418206dbace50b005af6fd
talipcr <talip.cesur@gmail.com>
2020-12-19 21:41:28 +0100

    update(package): update material

--- src/app/core/services/api.service.ts
+++ src/app/core/services/api.service.ts
@@ -1,33 +1,32 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { throwError } from 'rxjs';
+import { of, throwError } from 'rxjs';
 import { tap, catchError } from 'rxjs/operators';
-import { of } from 'rxjs/observable/of';
 import { TeamModel } from 'src/app/shared/store';
 
 @Injectable()
 export class ApiService {
-  constructor(private http: HttpClient) {}
+	constructor(private http: HttpClient) {}
 
-  someApiCall() {
-    return of([]).pipe(tap(() => console.log('log from service')));
-  }
+	someApiCall() {
+		return of([]).pipe(tap(() => console.log('log from service')));
+	}
 
-  public getAllTeams() {
-    return this.http.get('http://localhost:3000/team').pipe(
-      catchError((err: any) => {
-        console.log(err);
-        return throwError(err);
-      })
-    );
-  }
+	public getAllTeams() {
+		return this.http.get('http://localhost:3000/team').pipe(
+			catchError((err: any) => {
+				console.log(err);
+				return throwError(err);
+			})
+		);
+	}
 
-  public addPlayer(teamId: number, team: TeamModel) {
-    return this.http.patch(`http://localhost:3000/team/${teamId}`, team).pipe(
-      catchError((err: any) => {
-        console.log(err);
-        return throwError(err);
-      })
-    );
-  }
+	public addPlayer(teamId: number, team: TeamModel) {
+		return this.http.patch(`http://localhost:3000/team/${teamId}`, team).pipe(
+			catchError((err: any) => {
+				console.log(err);
+				return throwError(err);
+			})
+		);
+	}
 }
